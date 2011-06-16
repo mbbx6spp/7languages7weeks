4e2ff196639e4e8de8520f0e9dfafe7716cad2e2
Susan Potter <me@susanpotter.net>
2011-06-16 07:57:11 -0500

    Add remaining day two exercises and RSpec examples

--- ruby/day2/selfstudy.rb
+++ ruby/day2/selfstudy.rb
@@ -16,5 +16,56 @@ module DayTwo
         puts slice.join(' ')
       end
     end
+
+    def my_grepper(regex, *filenames)
+      results = {}
+      counter = 0
+      filenames.each do |fn|
+        line_matches = {}
+        counter += 1
+        current_line_no = 0
+        File.open(fn, 'r') do |line|
+          current_line_no += 1
+          if line =~ regex
+            line_matches[current_line_no] = line
+            puts "#{current_line_no}:#{line}"
+          end
+        end
+        results[fn] = line_matches
+      end
+      # Return something useful to the client code so that 
+      # they can do something useful with the results
+      results
+    end
+  end
+end
+
+class Tree
+  attr_accessor :children, :node_name
+
+  def initialize(hash)
+    raise "There can only be one root for a tree" unless hash.size === 1
+    (name, kids) = root = hash.first
+    @node_name = name
+    @children = []
+    kids.each do |kid|
+      @children << construct_child(kid)
+    end
+  end
+
+  def visit_all(&block)
+    visit(&block)
+    children.each {|c| c.visit_all(&block) }
+  end
+
+  def visit(&block)
+    block.call(self)
+  end
+
+  private
+
+  def construct_child(elem)
+    (name, kids) = elem
+    Tree.new(name => kids)
   end
 end
--- ruby/day2/selfstudy_spec.rb
+++ ruby/day2/selfstudy_spec.rb
@@ -78,4 +78,88 @@ describe "Day Two, Week One: Self-Study Exercises" do
       end
     end
   end
+
+  describe "Tree should accept a tree represented in a Hash" do
+    before(:all) do
+      @hash = {
+        'grandpa' => {
+          'dad' => { 'child 1' => {}, 'child 2' => {} },
+          'uncle' => { 'child 3' => {}, 'child 4' => {} }
+        }
+      }
+    end
+    describe "on initiallize" do
+      it "should raise an error if there is more than one element in the Hash - there can only be one root of a tree" do
+        lambda {
+          Tree.new('first' => {}, 'second' => {})
+        }.should raise_error
+      end
+  
+      it "should not raise ArgumentError when passing only one argument to initializer" do
+        lambda {
+          Tree.new(@hash)
+        }.should_not raise_error
+      end
+    end
+
+    describe "after initialized" do
+      before(:each) do
+        @tree = Tree.new(@hash)
+      end
+
+      it "should set node name of the root element as expected by given input" do
+        @tree.node_name.should == 'grandpa'  
+      end
+
+      it "should have exactly two children" do
+        @tree.should have_exactly(2).children
+      end
+
+      it "should have children of type Tree" do
+        # doesn't actually test if there are children though thus previous example
+        @tree.children.each {|child| child.should be_a(Tree) }
+      end
+
+      it "should have seven nodes in full tree" do
+        counter = 0
+        @tree.visit_all {|elem| counter += 1 }
+        counter.should == 7
+      end
+    end
+  end
+
+  describe "My grepper" do
+    before(:each) do
+      @playlist = [
+        "Ke$ha - Bla bla bla",
+        "Lady Gaga - Just Dance",
+        "Katy Perry - .....",
+        "Lady Gaga - Alejandro"
+      ]
+      @filename = "myplaylist"
+      File.stub!(:open).and_yield(@playlist[0]).
+        and_yield(@playlist[1]).
+        and_yield(@playlist[2]).
+        and_yield(@playlist[3])
+    end
+
+    it "should print matching lines to STDOUT" do
+      output = capture_streams(:stdout) do
+        DayTwo.my_grepper(/Gaga/, @filename)
+      end
+      output.should =~ /2:#{@playlist[1]}/
+      output.should =~ /4:#{@playlist[3]}/
+    end
+
+    it "should return expected result Hash" do
+      # To remove horrid puts from RSpec success/failure output
+      $stdout.stub!(:puts)
+      result = DayTwo.my_grepper(/Gaga/, @filename)
+      result.should have_exactly(1).entry
+      (key, value) = result.first
+      key.should == @filename
+      value.should have_exactly(2).items
+      # TODO: flesh out more if have time.
+    end
+  end
 end
